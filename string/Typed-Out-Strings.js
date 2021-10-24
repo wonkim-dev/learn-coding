70783ea5b367085a7e116f25e8ca817475d81010
wonkim-dev <wonkim.dev@gmail.com>
2021-10-24 15:36:05 +0200

    String: typed-out string optimization

--- string/Typed-Out-Strings.js
+++ string/Typed-Out-Strings.js
@@ -9,8 +9,8 @@
 // Two empty strings equal to each other.
 // It is case sensitive.
 
-const S = "ab#e";
-const T = "ak#t#e";
+const S = "ab#ef#"; // ae
+const T = "akf##e"; // ae
 
 // time & space complexity: O(n)
 function buildArray(string) {
@@ -26,9 +26,9 @@ function buildArray(string) {
 }
 
 // time & space complexity: O(2a + b) or O(a + 2b) => O(a + b)
-function backSpaceCompare(S, T) {
-  const finalS = buildArray(S); // time & space complexity: O(a)
-  const finalT = buildArray(T); // time & space complexity: O(b)
+function backSpaceCompare(string1, string2) {
+  const finalS = buildArray(string1); // time & space complexity: O(a)
+  const finalT = buildArray(string2); // time & space complexity: O(b)
   if (finalS.length !== finalT.length) {
     return false;
   }
@@ -42,3 +42,54 @@ function backSpaceCompare(S, T) {
 }
 
 console.log(backSpaceCompare(S, T));
+
+//--------------------------------------------------------------------
+
+/**
+ * In the while loop it touches all elements in the arrays only once
+ * by moving pointers to the left direction only => time complexity: O(a + b)
+ * There is no scaling variable but p1 and p2=> space complexity: O(1)
+ */
+function backSpaceCompare2(string1, string2) {
+  let p1 = string1.length - 1;
+  let p2 = string2.length - 1;
+
+  while (p1 >= 0 || p2 >= 0) {
+    if (string1[p1] !== "#" && string2[p2] !== "#") {
+      // check if the two values are identical
+      if (string1[p1] === string2[p2]) {
+        p1--; // moving to the left
+        p2--; // moving to the left
+      } else {
+        return false;
+      }
+    } else {
+      // do not compare the values, but move pointers to the left
+      // if one of the pointers has hash tag
+      if (string1[p1] === "#") {
+        let backwardCount = 2;
+        while (backwardCount > 0) {
+          p1--; // moving to the left
+          backwardCount--;
+          if (string1[p1] === "#") {
+            backwardCount += 2;
+          }
+        }
+      }
+
+      if (string2[p2] === "#") {
+        let backwardCount = 2;
+        while (backwardCount > 0) {
+          p2--; // moving to the left
+          backwardCount--;
+          if (string2[p2] === "#") {
+            backwardCount += 2;
+          }
+        }
+      }
+    }
+  }
+  return true;
+}
+
+console.log(backSpaceCompare2(S, T));
